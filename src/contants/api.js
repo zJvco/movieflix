b7d188758c853a6b35aa602824ca71850ba25127
Jvco <65981077+zJvco@users.noreply.github.com>
2022-07-10 22:35:11 -0300

    Adding .env file

--- src/contants/api.js
+++ src/contants/api.js
@@ -1,5 +1,5 @@
-const API_URL = "https://api.themoviedb.org/3";
-const API_TOKEN = "8401c3852d7ededd110d898d40858da8";
-const API_IMG = "https://image.tmdb.org/t/p/w500";
+const API_URL = process.env.REACT_APP_API_URL;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+const API_IMG = process.env.REACT_APP_API_IMG;
 
 export { API_URL, API_TOKEN, API_IMG };
